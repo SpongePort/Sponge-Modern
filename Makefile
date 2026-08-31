5dfa5a98fb8a0e6b6dbddf94e58fb1654914babf
TheFamiliarScoot <tperry2241@gmail.com>
2026-08-31 01:14:19 -0400

    Slight restructure

--- Makefile
+++ Makefile
@@ -1,13 +1,8 @@
 include common.mk
 
 TARGET = Spongey
-TYPE = ps-exe
-CFLAGS +=	-w \
-			-g \
-			-fpermissive \
-			$(COMMON_FLAGS)
-CXXFLAGS += -std=c++20 $(CFLAGS)
+CPPFLAGS +=	-w -g -fpermissive
 
-SRCS := $(wildcard $(SRC_DIR)/**/*.cpp))
+SRCS += $(wildcard src/**/*.cpp))
 
 include thirdparty/nugget/common.mk
--- common.mk
+++ common.mk
@@ -1,29 +1,40 @@
+# If you change this to exe, you'll have to rename the file ./thirdparty/nugget/ps-exe.ld too.
+TYPE = ps-exe
+
+THISDIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+
+SRCS += $(THISDIR)thirdparty/nugget/common/crt0/crt0.s
+SRCS += $(THISDIR)thirdparty/nugget/common/syscalls/printf.s 
+
+CPPFLAGS += -I$(THISDIR)thirdparty/nugget/psyq/include -I$(THISDIR)psyq-4_7-converted/include -I$(THISDIR)psyq-4.7-converted-full/include -I$(THISDIR)psyq/include 
+LDFLAGS += -L$(THISDIR)thirdparty/nugget/psyq/lib -L$(THISDIR)psyq-4_7-converted/lib -L$(THISDIR)psyq-4.7-converted-full/lib -L$(THISDIR)psyq/lib
+
 VERSION := DEBUG
 TERRITORY := USA
 USER := CDBUILD
 FILE_SYSTEM := PC
-PLATFORM := PC
 
-INCS :=  	-Iinclude -Isrc -Ipsyq/include
-
-DEFS :=  	-D__USER_$(USER)__ \
+CPPFLAGS += -Iinclude -Isrc \
+			-D__USER_$(USER)__ \
 			-D__VERSION_$(VERSION)__ \
 			-D__TERRITORY_$(TERRITORY)__ \
 			-D__FILE_SYSTEM_$(FILE_SYSTEM)__ \
 			-D__VERSION_STR__=$(VERSION) \
 			-D__TERRITORY_STR__=$(TERRITORY) \
 			-D__FILE_SYSTEM_STR__=$(FILE_SYSTEM) \
-			-DTARGET_${TARGET} \
 			-D_LANGUAGE_C_PLUS_PLUS
 
-LIBS :=     -Lpsyq/lib -Llib \
+LDFLAGS +=  -Llib \
+			-Wl,--start-group \
 			-lc -lapi -letc -lspu -lsnd \
             -lgpu -lcard -lpress \
             -lcd -ltap -lcomb -lmcrd \
-            -lgte -lpad -lds -lxmplay
+            -lgte -lpad -lds -lxmplay \
+			-Wl,--end-group
 
-COMMON_FLAGS := $(INCS) $(DEFS) $(LIBS)
 
-SRC_DIR := src
-BUILD_DIR := build
-PLATFORM_DIR := $(shell echo $(PLATFORM) | tr A-Z a-z)
+include $(THISDIR)thirdparty/nugget/common.mk
+
+define OBJCOPYME
+$(PREFIX)-objcopy -I binary --set-section-alignment .data=4 --rename-section .data=.rodata,alloc,load,readonly,data,contents -O $(FORMAT) -B mips $< $@
+endef
